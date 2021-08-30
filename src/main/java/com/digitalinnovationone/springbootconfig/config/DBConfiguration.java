db38c917a31bc044cb9d26b46061497e729658c5
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2021-08-30 17:47:28 -0300

    first commit

---
+++ src/main/java/com/digitalinnovationone/springbootconfig/config/DBConfiguration.java
@@ -0,0 +1,70 @@
+package com.digitalinnovationone.springbootconfig.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+
+public class DBConfiguration {
+    //mapeamento inicial
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //declarando metodos que fazem o mapeamento das propriedades
+    // para o nosso sistema e sobem o tipo de profile para nós
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection(){
+        System.out.println("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB connection to H2_TEST - Test instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String productionDataBaseConnection(){
+        System.out.println("DB connection for Production - MySQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB connection to MY_SQL_PROD - Production instance";
+    }
+
+}
